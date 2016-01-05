c46f1816f9625d5ef19b8523e3f0954e33226dd3
romanlutz <romanlutz13@gmail.com>
2016-01-05 16:21:14 +0100

    - Belady's algorithm with multiple cache sizes added

--- resources/beladys_algorithm.py
+++ resources/beladys_algorithm.py
@@ -2,7 +2,10 @@ import csv
 import heapq
 
 
-MAX_CACHE_SIZE = 500
+MAX_CACHE_SIZES = [100, 500]
+results = {}
+for s in MAX_CACHE_SIZES:
+    results[s] = []
 request_index, cache_hits = 0, 0
 
 traces = []
@@ -11,6 +14,8 @@ with open('trace_overview.csv', 'r') as trace_file:
     for line in csv_reader:
         traces.append(line[0])
         print line[0], ' \t',
+
+print ''
 
 occurrences = {}
 for trace_path in traces:
@@ -28,51 +33,56 @@ for trace_path in traces:
             else:
                 occurrences[object] = [request_index]
 
-    with open(trace_path, 'r') as trace:
-        csv_reader = csv.reader(trace)
-        # actual processing of workload
-        request_index = 0
-        cache_hits = 0
-        cache = {}
-        cache_size = 0
-        # next_occurrence will be underlying data structure of a max heap
-        # heapq library only supports min heaps, so every element will be negated
-        next_occurrence = []
-        for line in csv_reader:
-            request_index += 1
-            object = line[2]
+    for MAX_CACHE_SIZE in MAX_CACHE_SIZES:
+        with open(trace_path, 'r') as trace:
+            csv_reader = csv.reader(trace)
+            # actual processing of workload
+            request_index = 0
+            cache_hits = 0
+            cache = {}
+            cache_size = 0
+            # next_occurrence will be underlying data structure of a max heap
+            # heapq library only supports min heaps, so every element will be negated
+            next_occurrence = []
+            for line in csv_reader:
+                request_index += 1
+                object = line[2]
 
-            occurrences[object] = occurrences[object][1:]
+                occurrences[object] = occurrences[object][1:]
 
-            if object in cache:
-                cache_hits += 1
-                next_occurrence.remove((-request_index, object))
-                heapq.heapify(next_occurrence)
-                if occurrences[object] == []:
-                    # no future occurrences - remove element from cache
-                    cache_size -= 1
-                    del cache[object]
-                else:
-                    heapq.heappush(next_occurrence, (-occurrences[object][0], object))
-            else:
-                if cache_size < MAX_CACHE_SIZE:
+                if object in cache:
+                    cache_hits += 1
+                    next_occurrence.remove((-request_index, object))
+                    heapq.heapify(next_occurrence)
                     if occurrences[object] == []:
-                        # object won't occur any more
-                        pass
-                    else:
-                        cache[object] = True
-                        cache_size += 1
-                        heapq.heappush(next_occurrence, (-occurrences[object][0], object))
-                else:
-                    # replacement policy: replace the one which is needed the furthest in the future
-                    (_, evicted_object) = heapq.heappop(next_occurrence)
-                    del cache[evicted_object]
-
-                    if occurrences[object] == []:
-                        # newly cached object will not occur in the future
+                        # no future occurrences - remove element from cache
                         cache_size -= 1
+                        del cache[object]
                     else:
-                        cache[object] = True
                         heapq.heappush(next_occurrence, (-occurrences[object][0], object))
+                else:
+                    if cache_size < MAX_CACHE_SIZE:
+                        if occurrences[object] == []:
+                            # object won't occur any more
+                            pass
+                        else:
+                            cache[object] = True
+                            cache_size += 1
+                            heapq.heappush(next_occurrence, (-occurrences[object][0], object))
+                    else:
+                        # replacement policy: replace the one which is needed the furthest in the future
+                        (_, evicted_object) = heapq.heappop(next_occurrence)
+                        del cache[evicted_object]
 
-    print float(cache_hits) / float(request_index), ' \t',
+                        if occurrences[object] == []:
+                            # newly cached object will not occur in the future
+                            cache_size -= 1
+                        else:
+                            cache[object] = True
+                            heapq.heappush(next_occurrence, (-occurrences[object][0], object))
+
+            results[MAX_CACHE_SIZE.append(float(cache_hits) / float(request_index))
+
+
+for s in MAX_CACHE_SIZES:
+    print '\t'.join(results[s])
