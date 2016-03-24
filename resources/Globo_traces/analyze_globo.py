f0d8e7547a96d33c490d9321be47717d13e6fb30
Roman Lutz <romanlutz13@gmail.com>
2016-03-24 15:50:38 -0400

    - capture which file formats have cache hits

--- resources/Globo_traces/analyze_globo.py
+++ resources/Globo_traces/analyze_globo.py
@@ -1,7 +1,43 @@
 import getopt
 import sys, os
 from collections import defaultdict
+from datetime import datetime, timedelta
 from merge_globo import parse_line
+
+def time_difference(timestamp1, timestamp2):
+    year1, _, rest = timestamp1.partition('-')
+    month1, _, rest = rest.partition('-')
+    day1, _, rest = rest.partition('T')
+    hour1, _, rest = rest.partition(':')
+    min1, _, rest = rest.partition(':')
+    sec1, _, rest = rest.partition('-')
+    year2, _, rest = timestamp2.partition('-')
+    month2, _, rest = rest.partition('-')
+    day2, _, rest = rest.partition('T')
+    hour2, _, rest = rest.partition(':')
+    min2, _, rest = rest.partition(':')
+    sec2, _, rest = rest.partition('-')
+
+    [year1, year2, day1, day2, hour1, hour2, min1, min2, sec1, sec2] = \
+        map(int, [year1, year2, day1, day2, hour1, hour2, min1, min2, sec1, sec2])
+
+    # there are only January and Feburary requests, so this simple rule works
+    month1 = 1 if month1 == 'Jan' else 2
+    month2 = 1 if month2 == 'Jan' else 2
+
+    time1 = datetime(year1, month1, day1, hour1, min1, sec1)
+    time2 = datetime(year2, month2, day2, hour2, min2, sec2)
+
+    if time2 > time1:
+        delta = time2 - time1
+    else:
+        delta = time1 - time2
+
+    if delta.days == 0:
+        return delta.seconds
+    else:
+        return 86400
+
 
 def create_default_data_dict():
     data = {}
@@ -11,9 +47,15 @@ def create_default_data_dict():
     data['request_names'] = defaultdict(int)
     data['body_bytes_sizes'] = defaultdict(int)
     data['content_type'] = defaultdict(int)
+    data['content_type_hits'] = {}
     data['zero_bytes'] = 0
+    #data['last_requests'] = {}
 
     return data
+
+def clear_from_last_requests(timestamp, data):
+    for ip in data['last_requests']:
+        data['last_requests'][ip][:] = [request for request in data['last_requests'][ip] if time_difference(request['time'], timestamp) <= 10]
 
 def print_data_dict_compact(data):
     print 'IP/24 ranges:', len(data['ip_24_ranges'])
@@ -32,9 +74,11 @@ def print_data_dict_compact(data):
     for type in data['content_type']:
         print type, ':', data['content_type'][type], ',',
     print ''
+    print data['content_type_hits']
     print 'requests with 0 bytes:', data['zero_bytes']
 
 def analyze(path, day, month, year, data):
+    timestamp = '%d-%d-%dT00:00:00-02:00' % (year, month, day)
     for filename in os.listdir(path):
         if '%02d%02d%02d-merged.log' % (year, month, day) == filename:
             print 'reading %s' % filename
@@ -45,8 +89,7 @@ def analyze(path, day, month, year, data):
 
                     data['request_names'][request['http_request_name']] += 1
 
-                    if request['body_bytes_sent'] > 0 and request['http_request_name'] == 'GET' and \
-                                    request['code'] < 400:
+                    if request['http_request_name'] == 'GET' and request['code'] < 400:
                         data['zero_bytes'] += 1
 
                         ip_24 = ''.join(request['ip'][-1::-1].partition('.')[2])[-1::-1]
@@ -64,39 +107,50 @@ def analyze(path, day, month, year, data):
                             order_of_magnitude += 1
                         data['body_bytes_sizes']['10^%d < b < 10^%d' % (order_of_magnitude-1, order_of_magnitude)] += 1
 
+                        format = None
                         if 'mp4' in request['request_uri'] and request['request_uri'].partition('?')[0][-4:] == 'm3u8':
-                            data['content_type']['.mp4.m3u8'] += 1
+                            format = '.mp4.m3u8'
                         elif request['request_uri'].partition('?')[0][-4:] == '.mp4':
-                            data['content_type']['.mp4'] += 1
+                            format = '.mp4'
                         elif request['request_uri'].partition('?')[0][-3:] == '.ts':
-                            data['content_type']['.ts'] += 1
+                            format = '.ts'
                         elif request['request_uri'].partition('?')[0][-5:] == '.m3u8':
-                            data['content_type']['other .m3u8'] +=1
+                            format = 'other .m3u8'
                         elif 'QualityLevels' in request['request_uri'].partition('.ism')[2] and \
                              'Fragments' in request['request_uri'].partition('.ism')[2]:
-                            data['content_type']['.ism with QualityLevels and Fragments configuration'] += 1
+                            format = '.ism with QualityLevels and Fragments configuration'
                         elif '/manifest' == request['request_uri'].partition('.ism')[2].lower() or \
                                         request['request_uri'].partition('?')[0][-13:] == '.ism/manifest':
-                            data['content_type']['.ism/manifest'] += 1
+                            format = '.ism/manifest'
                         elif 'manifest' in request['request_uri'].partition('.ism')[2] and \
                              'Seg' in request['request_uri'].partition('.ism')[2] and \
                              'Frag' in request['request_uri'].partition('.ism')[2]:
-                             data['content_type']['.ism with manifest and Seg/Frag index'] += 1
+                            format = '.ism with manifest and Seg/Frag index'
                         elif request['request_uri'].partition('.ism')[1] == '.ism' and \
                              '.f4m' in request['request_uri'].partition('.ism')[2]:
-                             data['content_type']['.ism and .f4m'] += 1
+                            format = '.ism and .f4m'
                         elif request['request_uri'].partition('?')[0][-7:] == '.webvtt':
-                            data['content_type']['.webvtt'] += 1
+                            format = '.webvtt'
                         elif request['request_uri'].partition('?')[0][-4:] == '.mpd':
-                            data['content_type']['.mpd'] += 1
+                            format = '.mpd'
                         elif request['request_uri'].partition('?')[0][-4:] == '.srt':
-                            data['content_type']['.srt'] += 1
+                            format = '.srt'
                         elif request['request_uri'].partition('?')[0][-8:] == '.drmfaxs':
-                            data['content_type']['.drmfaxs'] += 1
+                            format = '.drmfaxs'
                         elif request['request_uri'].partition('?')[0][-8:] == '.drmmeta':
-                            data['content_type']['.drmmeta'] += 1
+                            format = 'drmmeta'
                         else:
                             print request
+
+                        if format is not None:
+                            data['content_type'][format] += 1
+                            if request['cache_hit_or_miss'] == 'HIT':
+                                data['content_type_hits'][format] = True
+
+                    # clear old records from last-requests dictionary to save memory
+                    # do this every 20 seconds because the last 10 seconds are saved
+                    #if time_difference(timestamp, request['time']) > 20:
+                    #    clear_from_last_requests(request['time'], data)
 
             print_data_dict_compact(data)
 
