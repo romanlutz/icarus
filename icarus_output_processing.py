0142c6a944ef568cb83896b2e7715fa31b3bdcbf
Roman Lutz <romanlutz13@gmail.com>
2016-05-15 22:32:11 -0400

    - config for Fastly and c=8000

--- icarus_output_processing.py
+++ icarus_output_processing.py
@@ -2,16 +2,17 @@ from icarus.results.visualize import draw_cache_level_proportions
 from icarus.results.output_results import print_cache_hit_rates, print_results_full
 
 def main():
+    file_prefix = 'results-weighted-ibm-c1000'
     #draw_cache_level_proportions('plots', 'results', '.spickle')
     #print_results_full('results-weighted-c1000-umass', '.spickle')
     print 'unweighted hit rate'
-    print_cache_hit_rates('results-weighted-c1000-umass', '.spickle', goal_tuple=('CACHE_HIT_RATIO', 'MEAN'), plot=False)
+    print_cache_hit_rates(file_prefix, '.spickle', goal_tuple=('CACHE_HIT_RATIO', 'MEAN'), plot=False)
     print 'normalized reward'
-    print_cache_hit_rates('results-weighted-c1000-umass', '.spickle', goal_tuple=('CACHE_HIT_RATIO', 'WEIGHTED_CACHE_HIT_RATIO'), plot=False)
+    print_cache_hit_rates(file_prefix, '.spickle', goal_tuple=('CACHE_HIT_RATIO', 'WEIGHTED_CACHE_HIT_RATIO'), plot=False)
     print 'weighted average hit rate'
-    print_cache_hit_rates('results-weighted-c1000-umass', '.spickle', goal_tuple=('CACHE_HIT_RATIO', 'WEIGHTED_CACHE_HIT_RATIO_SUM'), plot=False)
+    print_cache_hit_rates(file_prefix, '.spickle', goal_tuple=('CACHE_HIT_RATIO', 'WEIGHTED_CACHE_HIT_RATIO_SUM'), plot=False)
     print 'weight-frequency hit rate'
-    print_cache_hit_rates('results-weighted-c1000-umass', '.spickle', goal_tuple=('CACHE_HIT_RATIO', 'AVERAGE_BENEFIT'), plot=False)
+    print_cache_hit_rates(file_prefix, '.spickle', goal_tuple=('CACHE_HIT_RATIO', 'AVERAGE_BENEFIT'), plot=False)
 
 if __name__ == "__main__":
     main()
